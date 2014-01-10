d9b46950cbf4e8d2b0cd5054d508b9d767b01380
Daniel Santillan <daniel.santillan@eox.at>
2014-01-10 16:45:38 +0100

    Missing process definition

--- PyWPS-pywps-3.2.1/pywps/processes/__init__.py
+++ PyWPS-pywps-3.2.1/pywps/processes/__init__.py
@@ -1,5 +1,5 @@
 """Example process, which can be used with PyWPS"""
-__all__ = [ "test00" ]
+__all__ = [ "test00", "random_gen" ]
 # __all__ = ["exampleBufferProcess", "exampleXSLTProcess",
 #            "exampleBufferNoInputsProcess",
 #            "exampleLosProcess"]
